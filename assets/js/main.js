084b181a4e2615e949fd0c1457e1da8c2701e5f9
Bodoi <109707645+kieuduc123@users.noreply.github.com>
2023-07-25 23:47:22 +0700

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,15 @@ jQuery.fn.exists = function () {
 };
 
 var main = {
-    SlideScroll: function () {
-        $("#content-1").mCustomScrollbar({
-            scrollButtons: {enable: true},
-            theme: "3d",
-            scrollbarPosition: "outside"
-        });
-    },
     SliderHomePage: function () {
-        $('.list-partner').owlCarousel({
+        $('.slider-banner').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            items: 1,
+            dots: false
+        });
+        $(".list-banner-footer").owlCarousel({
             loop: true,
             margin: 10,
             nav: false,
@@ -20,39 +20,19 @@ var main = {
             autoplay: true
         });
 
-        $('.slider-banner').owlCarousel({
-            loop: true,
-            margin: 10,
-            nav: true,
-            items: 1,
-            dots: false
-        });
     },
 
-    Lightgallery: function () {
-        $("#lightgallery").lightGallery();
-        $("#video-player-param").lightGallery();
-
+    SlideScroll: function () {
+        $("#content-1").mCustomScrollbar({
+            theme: "3d",
+        });
     },
     Model: function () {
         $('#register-model').modal('show');
     },
-
-    Hover: function () {
-        $(".nav-item").hover(function () {
-            $(this).addClass('show');
-            $(this).children(".dropdown-menu").addClass('show');
-        }, function () {
-            $(this).removeClass('show');
-            $(this).children(".dropdown-menu").removeClass('show');
-        });
-    },
-
     Start: function () {
         this.SliderHomePage();
         this.SlideScroll();
-        this.Hover();
-        this.Lightgallery();
         this.Model();
         this.Skdslider();
 
@@ -61,42 +41,16 @@ var main = {
 $(document).ready(function () {
     main.Start();
 });
-jQuery('#skdslider').skdslider({
-
-    // Delay duration between two slides
+jQuery('.skdslider').skdslider({
+ // Delay duration between two slides
     'delay':5000, 
     'animationSpeed': 500,
-  
-    // Show navigation
     showNav: true,
-  
-    // 'showNextPrev': true, 
-    // 'numericNav': false,
-  
-    // Enable auto play
-    // 'autoSlide': true,
-  
-    // // Pause on hover
     'pauseOnHover':false,
-  
-    // // Show play button
     'showPlayButton':false,
-  
-    // // Stop sliding when reaching the last slide
     stopSlidingAfter: false,
     
-    // animationType: 'fading',
-    
-    // // Selector of slides
-    //slideSelector: '.slide',
-    
-    // // Active class
-    // activeClass: 'active',
-    
-    // callback
-   // onMarkup: function() {},
-//   
+  
   });
 
-new WOW().init();
 
